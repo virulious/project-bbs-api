d8d03f0f699b707c7e204ca7fcbdf4ba469cad83
Robert Cronin <Virulious@hotmail.com>
2020-05-14 14:49:06 -0400

    Change database
    Change database from project-bbs-api to project-2-api to fix bug

--- app/models/user.js
+++ app/models/user.js
@@ -13,10 +13,6 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
-  userName: {
-    type: String,
-    required: false
-  },
   token: String
 }, {
   posts: [postSchema]
